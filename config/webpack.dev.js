60e90337414577792ebf4895df2f2ffe840c1a12
Vũ Văn Thuỷ <macbook@MacBook-Pro-cua-MacBook.local>
2019-08-07 00:10:19 +0700

    express-final

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         main: ["./src/main.js"]
@@ -42,19 +43,7 @@ module.exports = {
                 test: /\.html$/,
                 use: [
                     {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].html",
-                        },
-                    },
-                    {
-                        loader: "extract-loader"
-                    },
-                    {
                         loader: 'html-loader',
-                        options: {
-                            attrs: ["img:src", "link:href"],
-                        }
                     },
 
                 ]
@@ -78,7 +67,10 @@ module.exports = {
 
         ]
     },
-    plugins : [
-        new webpack.HotModuleReplacementPlugin()
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(), 
+        new HTMLWebpackPlugin({
+            template : "./src/index.html",
+        })
     ]
 }
