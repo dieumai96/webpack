7cdc85283b8115bac1197181512970e6527fdfd1
Vu Thuy <vuthuycntt1996@gmail.com>
2019-08-12 14:39:35 +0700

    update

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,87 +6,90 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const helpers = require('./helpers');
 const isDev = process.env.NODE_ENV !== 'production';
-module.exports = {
-    entry: {
-        main: ["./src/main.js"]
-    },
-    mode: "production",
-    output: {
-        filename: "[name]-bundle.js",
-        path: path.resolve(__dirname, "../dist"),
-        publicPath: "/"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: "babel-loader"
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(css|scss|sass)$/,
-                use: [
-                    { loader: 'style-loader', options: { sourceMap: isDev } },
-                    { loader: 'css-loader', options: { sourceMap: isDev } },
-                    { loader: 'sass-loader', options: { sourceMap: isDev } }
-                ],
-                include: helpers.root('src', 'assets')
-            },
-            {
-                test: /\.(css|scss|sass)$/,
-                use: [
-                    'to-string-loader',
-                    { loader: 'css-loader', options: { sourceMap: isDev } },
-                    { loader: 'sass-loader', options: { sourceMap: isDev } }
-                ],
-                include: helpers.root('src', 'app')
-            },
-            {
-                // HTML LOADER
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
+module.exports = env => {
+    return {
+        entry: {
+            main: ["./src/main.js"]
+        },
+        mode: "production",
+        output: {
+            filename: "[name]-bundle.js",
+            path: path.resolve(__dirname, "../dist"),
+            publicPath: "/"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: [
+                        {
+                            loader: "babel-loader"
+                        }
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.(css|scss|sass)$/,
+                    use: [
+                        { loader: 'style-loader', options: { sourceMap: isDev } },
+                        { loader: 'css-loader', options: { sourceMap: isDev } },
+                        { loader: 'sass-loader', options: { sourceMap: isDev } }
+                    ],
+                    include: helpers.root('src', 'assets')
+                },
+                {
+                    test: /\.(css|scss|sass)$/,
+                    use: [
+                        'to-string-loader',
+                        { loader: 'css-loader', options: { sourceMap: isDev } },
+                        { loader: 'sass-loader', options: { sourceMap: isDev } }
+                    ],
+                    include: helpers.root('src', 'app')
+                },
+                {
+                    // HTML LOADER
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: 'html-loader',
+                        },
 
-                ]
-            },
-            // image loader
-            {
-                test: /\.(png|jpg|gif|woff(2)?|ttf|eot|svg)$/,
-                exclude: [
-                    /\.(js|jsx|mjs)$/,
-                    /\.html$/,
-                    /\.json$/,
-                    /\.(less|config|variables|overrides)$/,
-                ],
-                use: [
-                    {
-                        loader: 'file-loader'
-                    },
-                ],
-            },
+                    ]
+                },
+                // image loader
+                {
+                    test: /\.(png|jpg|gif|woff(2)?|ttf|eot|svg)$/,
+                    exclude: [
+                        /\.(js|jsx|mjs)$/,
+                        /\.html$/,
+                        /\.json$/,
+                        /\.(less|config|variables|overrides)$/,
+                    ],
+                    use: [
+                        {
+                            loader: 'file-loader'
+                        },
+                    ],
+                },
 
 
+            ]
+        },
+        plugins: [
+            // new ExtractTextPlugin("[name].css"),
+            new OptimizeCssAssetsPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "[name]-[contenthash].css"
+            }),
+            new HTMLWebpackPlugin({
+                template: "./src/index.html",
+            }),
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify(env.NODE_ENV)
+                }
+            }),
         ]
-    },
-    plugins: [
-        // new ExtractTextPlugin("[name].css"),
-        new OptimizeCssAssetsPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "[name]-[contenthash].css"
-        }),
-        new HTMLWebpackPlugin({
-            template: "./src/index.html",
-        }),
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production")
-            }
-        }),
-    ]
+    }
+
 }
