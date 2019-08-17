993e9fd90a33dc9d409b680bfd2540c5b89341a9
Vũ Văn Thuỷ <macbook@MacBook-Pro-cua-MacBook.local>
2019-08-17 22:03:41 +0700

    update

--- src/components/notifications/Notification.vue
+++ src/components/notifications/Notification.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container">
-    <!-- <div id="nb-global-spinner" class="spinner" v-if="isLoading">
+    <div id="nb-global-spinner" class="spinner" v-if="isLoading">
       <div class="blob blob-0"></div>
       <div class="blob blob-1"></div>title
       <div class="blob blob-2"></div>
       <div class="blob blob-3"></div>
       <div class="blob blob-4"></div>
       <div class="blob blob-5"></div>
-    </div>-->
+    </div>
     <md-table v-model="listData" md-card>
       <md-table-toolbar>
         <h1 class="md-title">Notifications</h1>
       </md-table-toolbar>
-      <!-- 
+
       <md-table-row slot="md-table-row" slot-scope="{ item }">
         <md-table-cell md-label="Action" md-sort-by="action" md-numeric>{{ item.id }}</md-table-cell>
         <md-table-cell md-label="Title" md-sort-by>{{ item.title }}</md-table-cell>
@@ -24,13 +24,13 @@
         <md-table-cell md-label="Send" md-sort-by="send">
           <md-checkbox v-model="item.status == 2" class="md-primary" disabled></md-checkbox>
         </md-table-cell>
-      </md-table-row>-->
+      </md-table-row>
     </md-table>
     <div class="container-fluid">
       <nav>
         <ul class="pagination">
           <li class="page-item">
-            <a class="page-link" aria-label=" Previous">
+            <a class="page-link" v-on:click="changePage('','prev')" aria-label=" Previous">
               <span aria-hidden="true">&laquo;</span>
             </a>
           </li>
@@ -39,10 +39,15 @@
             v-bind:key="item.id"
             v-for="(item,index) in renderPage.slice(0, 5)"
           >
-            <a class="page-link dpl-block" v-bind:class="{ active: item.page == 1 }">{{index + 1}}</a>
+            <a
+              class="page-link"
+              v-bind:class="{ active: item.page == 1 }"
+              v-bind:id="'dpl-block-'+item.page"
+              v-on:click="changePage(item.page,'page')"
+            >{{item.page}}</a>
           </li>
 
-          <li class="page-item">
+          <li class="page-item" v-on:click="changePage('','next')">
             <a class="page-link" aria-label="Next">
               <span aria-hidden="true">&raquo;</span>
             </a>
@@ -91,34 +96,11 @@ export default {
     }
   },
   created() {
-    const headers = {
-      Authorization: Config.TOKEN
-    };
-    // this.isLoading = true;
-    // axios
-    //   .post(
-    //     `${Config.API_ENDPOINT}/${Url.GET_ALL_NOTIFICATION}`,
-    //     {
-    //       page: 1,
-    //       limit: 10
-    //     },
-    //     { headers }
-    //   )
-    //   .then(res => {
-    //     this.isLoading = false;
-    //     if (res.status == 200) {
-    //       if (res.data.status == 0) {
-    //         let { data } = res.data;
-    //         console.log(data);
-    //         this.listData = data;
-    //       }
-    //     }
-    //   })
-    //   .catch(err => err);
     this.onSearch(1, {}, "first", null);
   },
   methods: {
     onSearch: function(page, searchObject, order, typeSearch) {
+      this.isLoading = true;
       const headers = {
         Authorization: Config.TOKEN
       };
@@ -140,16 +122,16 @@ export default {
               let data = res.data.data;
               if (data) {
                 this.listData = data;
-                console.log(this.listData);
                 this.totalCount = res.data.totalCount;
                 this.countPage = Math.ceil(this.totalCount / 10);
                 if (order == "next" && this.pageNow >= 5) {
-                  this.renderPage = this.renderTempPage.filter(
-                    e => e.page >= this.pageNow - 4
+                  this.renderPage = this.renderTempPage.filter(e => e.page >= this.pageNow - 4
                   );
+                  console.log(this.renderTempPage);
                   setTimeout(() => {
-                    $(`.dpl-block-${this.pageNow}`).addClass("active");
+                    $(`#dpl-block-${this.pageNow}`).addClass("active");
                   }, 250);
+                  console.log("cc",this.renderPage);
                 }
                 if (
                   order == "prev" &&
@@ -161,7 +143,7 @@ export default {
                     e => e.page >= max - 3
                   );
                   setTimeout(() => {
-                    $(`.dpl-block-${this.pageNow}`).addClass("active");
+                    $(`#dpl-block-${this.pageNow}`).addClass("active");
                   }, 250);
                 }
                 if (order == "first") {
@@ -175,11 +157,39 @@ export default {
                     });
                   }
                 }
-                console.log(this.renderPage);
               }
             }
           }
         });
+    },
+    changePage: async function(i, type) {
+      let body = {};
+      if (this.bodySearch) {
+        body = this.bodySearch;
+      }
+      if (type == "prev") {
+        if (this.pageNow != 1) {
+          for (let i = 1; i <= this.countPage; i++) {
+            $(`#dpl-block-${i}`).removeClass("active");
+          }
+          $(`#dpl-block-${this.pageNow - 1}`).addClass("active");
+          this.onSearch(this.pageNow - 1, body, type);
+        }
+      } else if (type == "next") {
+        if (this.pageNow != this.countPage) {
+          for (let i = 1; i <= this.countPage; i++) {
+            $(`#dpl-block-${i}`).removeClass("active");
+          }
+          $(`#dpl-block-${this.pageNow + 1}`).addClass("active");
+          this.onSearch(this.pageNow + 1, body, type);
+        }
+      } else {
+        await this.onSearch(i, body, "not-first");
+        for (let i = 1; i <= this.countPage; i++) {
+          $(`#dpl-block-${i}`).removeClass("active");
+        }
+        $(`#dpl-block-${i}`).addClass("active");
+      }
     }
   }
 };
