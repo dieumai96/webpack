20d5c5829f853fc28d721788fc753454de87a906
Vũ Văn Thuỷ <macbook@MacBook-Pro-cua-MacBook.local>
2019-08-27 22:39:57 +0700

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-
+import store from './store';
 Vue.use(VueMaterial)
 
 import router from './router';
@@ -12,5 +12,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
